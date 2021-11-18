5751e85023dea0e066df9d78f62b2598239ce305
Alex Sentinella <u0148373@kuleuven.be>
2021-11-18 12:59:01 +0100

    Adding a script that adds a and b

---
+++ R/test-script.R
@@ -0,0 +1,21 @@
+#This is a test script
+
+#Declare some variables
+a <- 4
+
+b <- 7
+
+#Make a simple function
+test_function <- function(a, b){
+  
+  c <- a + b
+  
+  return(c)
+}
+
+#Run the function
+c <- test_function(a, b)
+  
+#Print c
+c
+
