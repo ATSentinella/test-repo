89fb7f3a59e87877255fe92495ced38433ab0e07
Alex Sentinella <u0148373@kuleuven.be>
2021-12-16 14:19:00 +0100

    Changes values and function

--- R/test-script.R
+++ R/test-script.R
@@ -1,14 +1,14 @@
 #This is a test script
 
 #Declare some variables
-a <- 5 #a is now 5
+a <- 15 #a is now 5
 
-b <- 8 #b is now 8
+b <- 82 #b is now 8
 
 #Make a simple function
 test_function <- function(a, b){
   
-  c <- a * b
+  c <- a + b
   
   return(c)
 }
